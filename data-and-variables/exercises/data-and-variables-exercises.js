88fed16fadc098b051fbdca73501522973ff588e
Arielle Waks <ariellewaks@Arielles-Air.lan>
2023-09-11 14:03:26 -0500

    fixed syntax

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,33 +1,34 @@
 // Declare and assign the variables below
-let ShuttleName = "Determination"
-let ShuttleSpeed_MPH = 17500
-let DistanceMars_KM = 225000000
-let DistanceMoon_KM = 384400
-const MilesPerKM = 0.621;
+let ShuttleName = "Determination";
+let ShuttleSpeedMph = 17500;
+let DistanceMarsKm = 225000000;
+let DistanceMoonKm = 384400;
+const MilesPerKm = 0.621;
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
 
-console.log(typeof ShuttleName)
-console.log(typeof ShuttleSpeed_MPH)
-console.log(typeof DistanceMars_KM)
-console.log(typeof DistanceMoon_KM)
-console.log(typeof MilesPerKM)
+console.log(typeof ShuttleName);
+console.log(typeof ShuttleSpeedMph);
+console.log(typeof DistanceMarsKm);
+console.log(typeof DistanceMoonKm);
+console.log(typeof MilesPerKm);
 
 // Calculate a space mission below
 
-let DistanceMars_Miles = DistanceMars_KM*MilesPerKM
-let DistanceMars_Hours = DistanceMars_Miles/ShuttleSpeed_MPH
-let DistanceMars_Days = DistanceMars_Hours/24
+let DistanceMarsMiles = DistanceMarsKm*MilesPerKm;
+let DistanceMarsHours = DistanceMarsMiles/ShuttleSpeedMph;
+let DistanceMarsDays = DistanceMarsHours/24;
 
 // Print the results of the space mission calculations below
 
-console.log(ShuttleName+" will take "+DistanceMars_Days+" days to reach Mars.")
+console.log(ShuttleName + " will take " + DistanceMarsDays + " days to reach Mars.");
 
 // Calculate a trip to the moon below
 
-let DistanceMoon_Miles = DistanceMoon_KM*MilesPerKM
-let DistanceMoon_Hours = DistanceMoon_Miles/ShuttleSpeed_MPH
-let DistanceMoon_Days = DistanceMoon_Hours/24
+let DistanceMoonMiles = DistanceMoonKm * MilesPerKm;
+let DistanceMoonHours = DistanceMoonMiles / ShuttleSpeedMph;
+let DistanceMoonDays = DistanceMoonHours / 24;
 
 // Print the results of the trip to the moon below
-console.log(ShuttleName + " will take "+DistanceMoon_Days+" days to reach the Moon.");
+console.log(ShuttleName + " will take " + DistanceMoonDays + " days to reach the Moon.");
+
