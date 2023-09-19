ba5a548b75042c07cb670387a3721f56fdbdf0bb
Arielle Waks <ariellewaks@Arielles-Air.lan>
2023-09-18 22:03:52 -0500

    question 5 completed

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -19,9 +19,16 @@ const input = require('readline-sync');
 let question2 = input.question(`Which cabinet (0 - 3) would you like to open?`);
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-if (question2 === 0 || 1 || 2 || 3) {
+if (question2 === '0' || question2 === '1' || question2 === '2' || question2 === '3') {
     console.log(`You've opened the cabinet containing ${cargoHold[question2]}! `);
+    let question3 = input.question("Which of these items would you like to retrieve?");
+    if (cargoHold[question2].includes(question3)) {
+        console.log(`Cabinet ${question2} DOES contain ${question3}`);
+    } else {
+        console.log(`Cabinet ${question2} DOES NOT contain ${question3}`);
+    }
 } else {
     console.log(`error: ${question2} is an invalid answer`);
 }
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+// let question3 = input.question("Which of these items would you like to retrieve?");
