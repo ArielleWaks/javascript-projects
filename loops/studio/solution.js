9a5e53d1f891338ed12f8d82066789eb08b534bb
Arielle Waks <ariellewaks@Arielles-Air.lan>
2023-09-21 16:30:04 -0500

    arrays initialized

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -2,11 +2,11 @@ const input = require('readline-sync');
 
 // Part A: #1 Populate these arrays
 
-let protein = [];
-let grains = [];
-let veggies = [];
-let beverages = [];
-let desserts = [];
+let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+let veggies = ['peas', 'green beans', 'kale', 'edamame', 'broccoli', 'asparagus'];
+let beverages = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
+let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'];
 
 
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
@@ -15,7 +15,9 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
-
+  for (let i = 0; i < pantry.length; i++) {
+    meals += [pantry[0][i], pantry[1][i], pantry[2][i], pantry[3][i], pantry[4][i]];
+  }
 
   return meals;
 }
@@ -25,6 +27,9 @@ function askForNumber() {
   numMeals = input.question("How many meals would you like to make?");
   
   /// CODE YOUR SOLUTION TO PART B here ///
+  while (Number(numMeals) > 6 || Number(numMeals) < 1 || isNaN(numMeals)) {
+    numMeals = input.question("How many meals would you like to make?");
+  }
 
   return numMeals;
 }
@@ -45,8 +50,9 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  // console.log(meals)
+let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+console.log(meals);
+
   
 
   /// TEST PART B HERE ///
