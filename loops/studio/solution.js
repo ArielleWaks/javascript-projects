2c11c18a8bcd815debf7d60bcf7c3f018f4b5be5
Arielle Waks <ariellewaks@Arielles-MacBook-Air.local>
2023-09-21 20:30:24 -0500

    part A and B tests passed

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -7,29 +7,40 @@ let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
 let veggies = ['peas', 'green beans', 'kale', 'edamame', 'broccoli', 'asparagus'];
 let beverages = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
 let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'];
+let pantry = [protein, grains, veggies, beverages, desserts];
 
 
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   let pantry = [protein, grains, veggies, beverages, desserts];
   let meals = [];
+  //numMeals = 6;
   
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
-  for (let i = 0; i < pantry.length; i++) {
-    meals += [pantry[0][i], pantry[1][i], pantry[2][i], pantry[3][i], pantry[4][i]];
-  }
+  // for (let i = 0; i < numMeals; i++) {
+    for (let j = 0; j < numMeals; j++) {
+      let meal = [];
+      meal.push(pantry[0][j], pantry[1][j], pantry[2][j], pantry[3][j], pantry[4][j]);
+      meals.push(meal);
+    }
+    // let meal = [pantry[0][i], pantry[1][i], pantry[2][i], pantry[3][i], pantry[4][i]];
+    
+    //console.log(meals);
+
+  // }
 
   return meals;
+
 }
 
-
 function askForNumber() {
-  numMeals = input.question("How many meals would you like to make?");
+  numMeals = input.question("How many meals would you like to make? ");
   
   /// CODE YOUR SOLUTION TO PART B here ///
   while (Number(numMeals) > 6 || Number(numMeals) < 1 || isNaN(numMeals)) {
-    numMeals = input.question("How many meals would you like to make?");
+    numMeals = input.question("How many meals would you like to make? ");
   }
+//numMeals = 
 
   return numMeals;
 }
@@ -39,6 +50,9 @@ function generatePassword(string1, string2) {
   let code = '';
 
   /// Code your Bonus Mission Solution here ///
+  for (i = 0; i < string1.length || i < string2.length || i++) {
+    
+  }
 
   return code;
 }
@@ -59,16 +73,16 @@ console.log(meals);
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
   
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+console.log(mealsForX);
 
     /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
 
-  // let password1 = '';
-  // let password2 = '';
-  // console.log("Time to run the password generator so we can update the menu tomorrow.")
-  // console.log(`The new password is: ${generatePassword(password1, password2)}`);
+let password1 = '1234';
+let password2 = 'ABCD';
+console.log("Time to run the password generator so we can update the menu tomorrow.")
+console.log(`The new password is: ${generatePassword(password1, password2)}`);
 }
 
 module.exports = {
