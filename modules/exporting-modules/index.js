84000eb7e4b8bf6f3283a9b3dfc65c0fb1f3e354
Arielle Waks <ariellewaks@Arielles-Air.lan>
2023-10-04 15:11:31 -0500

    exercises and chapter examples completed

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,18 @@
 // Import the modules exported from practiceExports.js below:
+
+const practice = require('./practiceExports');
+
+console.log(typeof practice); // object
+// console.log(practice);
+
+//to call one of the functions, use practice.functionName(argument)
+
+let arr = ['hello', 'world', 123, 987, 'LC101'];
+
+console.log(practice.isPalindrome('mom'));
+
+console.log(practice.evenOrOdd(99));
+
+for (i = 0; i < 3; i++) {
+    console.log(practice.randomArrayElement(arr));
+}
--- modules/exporting-modules/practiceExports.js
+++ modules/exporting-modules/practiceExports.js
@@ -14,3 +14,10 @@ function randomArrayElement(arr){
     let index = Math.floor(Math.random()*arr.length);
     return arr[index];
 }
+
+// module.exports = isPalindrome; // makes the isPalindrome function available to other files
+module.exports = { // to export multiple functions, create an object with this syntax
+    isPalindrome: isPalindrome, // the keys will be the names used in a different file to call the functions, and the values are the functions themselves
+    evenOrOdd: evenOrOdd,
+    randomArrayElement: randomArrayElement
+}
